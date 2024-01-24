44382d7a02864238eed0ed93780f1dc7623cbad5
Starry-OvO <starry.qvq@gmail.com>
2024-01-24 12:46:24 +0800

    feat: split bench for cmp

---
+++ app/bench_ocv.cpp
@@ -0,0 +1,29 @@
+#include <chrono>
+#include <iostream>
+
+#include <opencv2/img_hash.hpp>
+#include <opencv2/imgcodecs.hpp>
+
+#include "imghash/average_hash/avx2/impl.hpp"
+
+int main()
+{
+    auto src = cv::imread("sample.jpg");
+    std::cout << "src width: " << src.cols << '\n';
+    std::cout << "src height: " << src.rows << '\n';
+    std::cout << "src channels: " << src.channels() << '\n';
+    std::cout << "src step: " << src.step << '\n';
+    std::cout << '\n';
+
+    /* OpenCV ahash */
+    auto ptr_hasher = cv::img_hash::AverageHash::create();
+    cv::Mat res2;
+    auto start = std::chrono::high_resolution_clock::now();
+    ptr_hasher->compute(src, res2);
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+
+    std::cout << "OpenCV hash: " << res2;
+    std::cout << '\n';
+    std::cout << "time cost: " << (double)duration / 1000000 << " ms" << std::endl;
+}
---
+++ app/bench_this.cpp
@@ -0,0 +1,31 @@
+#include <chrono>
+#include <iostream>
+
+#include <opencv2/img_hash.hpp>
+#include <opencv2/imgcodecs.hpp>
+
+#include "imghash/average_hash/avx2/impl.hpp"
+
+int main()
+{
+    auto src = cv::imread("sample.jpg");
+    std::cout << "src width: " << src.cols << '\n';
+    std::cout << "src height: " << src.rows << '\n';
+    std::cout << "src channels: " << src.channels() << '\n';
+    std::cout << "src step: " << src.step << '\n';
+    std::cout << '\n';
+
+    /* My ahash */
+    uint8_t res[8];
+    auto start = std::chrono::high_resolution_clock::now();
+    igh::ahash::compute_ch3_div8(src.data, src.cols, src.rows, (int)src.step, res);
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+
+    std::cout << "My hash: ";
+    for (uint8_t i : res) {
+        std::cout << (uint16_t)i << " ";
+    }
+    std::cout << '\n';
+    std::cout << "time cost: " << (double)duration / 1000000 << " ms\n";
+}
